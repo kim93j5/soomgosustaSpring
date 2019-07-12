673390e99dee4fb877b11b73c09bdec346046259
nayunjoo <najjoo0819@hanmail.net>
2019-07-12 17:38:00 +0900

    nano124124 commit

---
+++ Soomgosusta/src/main/java/kosta/soomgosusta/controller/PartController.java
@@ -0,0 +1,5 @@
+package kosta.soomgosusta.controller;
+
+public class PartController {
+
+}
---
+++ Soomgosusta/src/main/java/kosta/soomgosusta/domain/PartVO.java
@@ -0,0 +1,12 @@
+package kosta.soomgosusta.domain;
+
+import lombok.Data;
+
+@Data
+public class PartVO {
+	int pSeq;
+	String pLWord;
+	String pMWord;
+	String pSWord;
+	String pImage;
+}
